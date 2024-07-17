549b5a25c0f6eef78fc29856854f8d19309a496e
xyzbtw <89705569+xyzbtw@users.noreply.github.com>
2024-07-16 20:55:26 -0600

    Update MoverPlugin.java

--- src/main/java/dev/xyzbtw/MoverPlugin.java
+++ src/main/java/dev/xyzbtw/MoverPlugin.java
@@ -11,8 +11,6 @@ public class MoverPlugin extends Plugin {
 		final StashMover stashMoverModule = new StashMover();
 		RusherHackAPI.getModuleManager().registerFeature(stashMoverModule);
 
-		final NewChunks newChunksModule = new NewChunks();
-		//RusherHackAPI.getModuleManager().registerFeature(newChunksModule);
 
 	}
 	
